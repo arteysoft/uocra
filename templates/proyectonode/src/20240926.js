7e856ae97e5633613f1b93cb4cc323d598ecf0a3
andy <arteysoft@gmail.com>
2024-09-26 21:05:50 -0300

    20240926_2105

--- templates/proyectonode/src/20240926.js
+++ templates/proyectonode/src/20240926.js
@@ -1,5 +1,7 @@
 let genUsu = require('./generarusuario')
+let fs = require('fs')
 
+const CARPETA_ARHCIVOS_INPUT = "c:\\input\\"
 
 let generarMilUsuarios = () => {
     let arrUsuarios = []
@@ -15,13 +17,49 @@ let convertiraJson_pura = (obj) => {
     return JSON.stringify(obj, null, 4)
 }
 
+let obtenerPathInput = (id) => {
+    return [CARPETA_ARHCIVOS_INPUT, id, ".json"].join('')
+}
+
+let guardarArchivoInput = (usu) => {
+    console.log(usu)
+    let strUsu = convertiraJson_pura(usu)
+    fs.writeFileSync(obtenerPathInput(usu.id), strUsu, 'utf8')
+}
+
+let guardarArrUsuariosInput = (usuarios) => {
+    for (let item of usuarios) {
+        guardarArchivoInput(item)        
+    }
+}
+
 let visualizarUsuarios = (usuarios) => {
     for (let item of usuarios) {
         console.log(convertiraJson_pura(item))
     }
 }
 
-module.exports = () => {        
-    let xs = generarMilUsuarios()
-    visualizarUsuarios(xs)    
+///////////////////////////////////////////////////
+
+let leerTodosLosUsuarios = () => {
+    // fs.readdirSync    
+    // return un array con todos los usuarios en binario
+    // for .. of
+    // leo cada uno de los archivos
+    JSON.parse('{"nombre":"andy"}')
+    // le aplico sobre amount el 5%
+    // en otro campo que se llame inpuesto
+    // 1) Genero el out en otro array ?
+    // 2) Lo descargo directo a la carpeta output con el mismo nombre
+}
+
+
+
+
+
+module.exports = () => {            
+    // let xs = generarMilUsuarios()
+    // let s = visualizarUsuarios(xs)
+    // guardarArrUsuariosInput(xs)
+    leerTodosLosUsuarios()
 }
