9dff6cd7c825b8796a6cd3ba037a457cf6f7d80d
Andy <arteysoft@gmail.com>
2024-10-02 16:36:08 -0300

    agrego proyecto integrador

---
+++ proyectointegrador/src/index.ts
@@ -0,0 +1,41 @@
+import 'dotenv/config'
+
+switch (process.argv[2]) {
+    case 'creararchivos':        
+        break
+    case 'discoverNumerosPrimos':        
+        break
+    case 'probar':        
+        break
+    case 'server':        
+        break
+    case 'cliente':        
+        break
+    case 'levantararchivos':        
+        break
+    case 'levantararchivosmongo':        
+        break
+    case 'collatz':        
+        break
+    case 'pruebamongo':        
+        break
+    case 'pruebapasswords':        
+        break
+    case 'crearjwt':        
+        break
+    case 'verificarjwt':        
+        break
+    case 'leerjwt':        
+        break
+    case 'servidorsito':        
+        break
+    case 'pruebalog':        
+        break
+    case 'crearclientesmongo':        
+        break    
+    default:
+        console.log('Atencion, se debe enviar un parametro con la accion a seguir')
+}
+
+
+
