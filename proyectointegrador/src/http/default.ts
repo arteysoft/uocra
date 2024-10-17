fbb0cc506b1eaa4efca0d1bd29f98d3e8819ccf1
andy <arteysoft>
2024-10-17 20:46:20 -0300

    pre azul

--- proyectointegrador/src/http/default.ts
+++ proyectointegrador/src/http/default.ts
@@ -1,18 +1,31 @@
 import express from 'express'
+const path = require('path');
+import datosRouter  from './routes/datosRouter'
 
 export default () => {
 
     console.log('Levantando servidor HTTP')
     let app = express()
 
+    console.log(path.join(__dirname, '../../public'))
+
+    app.use(express.static(
+        path.join(__dirname, '../../public')));
+
+    app.use('/api/datos', datosRouter)
+
+    app.get("/hola", (req, res) => {
+        res.send("hola").end()
+    })
+
     app.listen(80, () => {
-        console.log('escuchando puerto 3000')
+        console.log('escuchando puerto 80')
     })
 
     /*
     logger.info('levantando express')
     
-    app.use(express.static(path.join(__dirname, '../../public')));
+    
 
     // app.use(express.json());
     // app.use('/api/login', loginMiddleware)
---
+++ proyectointegrador/src/http/routes/datosRouter.ts
@@ -0,0 +1,46 @@
+import express from 'express'
+
+// Gaussian (Normal) Distribution Formula
+function gaussian(x, mean, standardDeviation) {
+    const exponent = -((x - mean) ** 2) / (2 * standardDeviation ** 2);
+    const coefficient = 1 / (standardDeviation * Math.sqrt(2 * Math.PI));
+    return coefficient * Math.exp(exponent);
+}
+
+/*
+// Example usage
+const mean = 0;  // Mean (μ)
+const standardDeviation = 1;  // Standard deviation (σ)
+
+const x = 1;  // Example input value
+const result = gaussian(x, mean, standardDeviation);
+*/
+
+
+export default express.Router()
+    .use('', (request, response, next) => {
+        console.log('Pasando por el middleware de hola')
+        next()
+    })    
+    .get('/ej1', async (request, response) => {
+        let datos:number[] = []
+        for (let idx = 1; idx <= 10; idx++) {
+            let newData = gaussian(idx, 5, 1.20) + 1
+            newData = Math.round(newData * 1000)
+            console.log(newData)
+            datos.push(newData)
+        }
+
+        try {
+            response            
+            .status(200)
+            .send(datos)
+        }
+        catch(err) {
+            response            
+            .status(500)
+            .send()
+        }
+    })
+    
+
