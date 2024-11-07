1ea66373e10c3970258b7477542bf010b4557656
Your Name <you@example.com>
2024-11-07 20:23:28 -0300

    ejercicio

--- generaDatosUsuariosSQL/src/driverPromise.ts
+++ generaDatosUsuariosSQL/src/driverPromise.ts
@@ -33,7 +33,7 @@ export let connect = (connection) => {
     })
 }
 
-export let query = async (connection, strSQL, arrValues) => {    
+export let query = async (connection, strSQL, arrValues:(string | number)[] = []) => {
     return new Promise((resolve, reject) => {
         connection.query(strSQL, arrValues, (err, resultset) => {
             if (err) {
--- generaDatosUsuariosSQL/src/index.ts
+++ generaDatosUsuariosSQL/src/index.ts
@@ -14,10 +14,27 @@ let fnInit = async () => {
     await cerrarConexion(conn)
 }
 
-fnInit()
-    .then()
-    .catch()
+let fnSeleccionChoferes = async () => {
+    let conn = crearConexion()   
+    let sqlSelect = "SELECT * FROM choferes WHERE cantidad_de_accidentes > ?"
+    let resultSet = await query(conn, sqlSelect, [0])
+    console.log(resultSet)
+    await cerrarConexion(conn)
+}
 
+/*
+La tarea es: desde SQL diferenciar aquellos que les gusta su trabajo
+y los que no
+
+Una vez hecho eso ? cambiamos y lo hacemos con filter en javascript
+traemos todo y lo filtramos en nuestro host
+*/
+
+
+
+fnSeleccionChoferes()
+.then()
+.catch()
 
 
 
